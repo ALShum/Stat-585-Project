5d76112da2c850c24b7b4aa3b467112a041fad36
Alex <ashum86@gmail.com>
2014-04-07 18:35:08 -0500

    university data

---
+++ school.R
@@ -0,0 +1,41 @@
+#B14004
+# Sex by College or Graduate School Enrollment 
+#by Type of School by Age for the Population 15+ Yrs
+library(acs)
+library(reshape2)
+library(plyr)
+library(ggplot2)
+states = geo.make(state = "*")
+
+get_pct = function(df) {
+  toReturn = data.frame(state = df$state,
+                        public = (df[,3] + df[,19])/
+                                  (df[,3] + df[,8] + 
+                                     df[,19] + df[,24]))
+  names(toReturn) = c("state", "public")
+  return(toReturn)
+}
+
+dat = data.frame(acs.fetch(endyear = 2012, span = 5, geography = states, table.number = "B14004", col.names="pretty")@estimate)
+names(dat) = gsub("Sex.by.College.or.Graduate.School.Enrollment.by.Type.of.School.by.Age.for.the.Population.15..Yrs..", "", names(dat))
+dat$state = rownames(dat)
+rownames(dat) = NULL
+univ.orig = dat
+dat = dat[,-c(1,2,3,8,13,18,19,24,29)]
+dat.melt = melt(dat, id = 'state')
+dat.split = ldply(strsplit(as.character(dat.melt$variable), "\\.\\."))
+#univ.orig = dataframe unmelted#
+
+univ = data.frame(state = dat$state, 
+                  gender = dat.split$V1,
+                  school = dat.split$V2,
+                  age = dat.split$V3,
+                  freq = dat.melt$value)
+levels(univ$school) = c("private", "public", "not enrolled")
+univ$age = gsub(".years", "", univ$age)
+#univ = dataframe reshaped#
+
+
+qplot(public, reorder(state, public), data = get_pct(univ.orig))
+
+
