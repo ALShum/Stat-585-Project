efc9abc15410be6619f2754964dee5ee0da92916
James Curro <jamescurro@gmail.com>
2014-04-09 12:10:17 -0500

    clean health data

--- healthinsurance.R
+++ healthinsurance.R
@@ -1,7 +1,51 @@
-#B27001 Health Insurance Coverage Status by Sex by Age
-#B27015 Health Insurance Coverage Status and Type by Household Income in the Past 12 Months
 #C27001A-H Health Insurance Coverage Status by Age (RACE)
 
 library(acs)
+library(ggplot2)
+library(reshape2)
+library(plyr)
+library(dplyr)
+
+#B27001 Health Insurance Coverage Status by Sex by Age
 states = geo.make(state = "*")
 health = acs.fetch(endyear = 2012, span = 5, geography = states, table.number = "B27001", col.names="pretty")
+health.df = data.frame(health@estimate)
+names(health.df) = gsub("Health.Insurance.Coverage.Status.by.Sex.by.Age..", "", names(health.df))
+names(health.df) = gsub(".health.insurance.coverage", "", names(health.df))
+health.df = health.df[,-c(1,2,30)]
+health.df = health.df[,-seq(1,52,3)]
+health.df$state = rownames(health.df)
+rownames(health.df) = NULL
+health.melt = melt(health.df, id='state')
+healthcols = ldply(strsplit(as.character(health.melt$variable), "\\.\\."))
+healthsex = data.frame(state = health.melt$state, gender = healthcols$V1, 
+                 age = healthcols$V2, coverage = healthcols$V3, freq = health.melt$value)
+healthsex$age = factor(healthsex$age, levels(healthsex$age)[c(9,6,1:5,7,8)])
+
+
+
+#B27015 Health Insurance Coverage Status and Type by Household Income in the Past 12 Months
+states = geo.make(state = "*")
+health = acs.fetch(endyear = 2012, span = 5, geography = states, table.number = "B27015", col.names="pretty")
+health.df = data.frame(health@estimate)
+names(health.df) = gsub("Health.Insurance.Coverage.Status.and.Type.by.Household.Income.in.the.Past.12.Months..", "", names(health.df))
+names(health.df) = gsub("health.insurance.|.health.insurance|.coverage|coverage.|With.", "", names(health.df))
+names(health.df) = gsub("Under..", "Under.", names(health.df))
+names(health.df) = gsub("to..", "to.", names(health.df))
+health.df = health.df[,-c(1,seq(2,22,5),seq(3,23,5))]
+health.df$state = rownames(health.df)
+rownames(health.df) = NULL
+health.melt = melt(health.df, id='state')
+healthcols = ldply(strsplit(as.character(health.melt$variable), "\\.\\."))
+healthincome = data.frame(state = health.melt$state, income = healthcols$V1, 
+                       coverage = healthcols$V2, freq = health.melt$value)
+
+
+
+
+
+
+
+
+
+
