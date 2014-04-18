5bce01fd6eca56ce566c83f1b3d242e4de44e204
Alex <ashum86@gmail.com>
2014-04-18 15:23:08 -0500

    test

--- Shiny/server.r
+++ Shiny/server.r
@@ -3,4 +3,12 @@ library(acs)
 
 shinyServer(function(input, output, session) {
   
+  
+  output$mpgPlot <- renderPlot({
+    boxplot(as.formula(formulaText()), 
+            data = mpgData,
+            outline = input$outliers)
+  })
+  
+  
 })
