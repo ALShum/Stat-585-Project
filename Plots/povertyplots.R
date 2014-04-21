b5d33bfff2f72dcb164b69de9f122a7ae03ff609
James Curro <jamescurro@gmail.com>
2014-04-21 16:57:33 -0500

    idk

--- Plots/povertyplots.R
+++ Plots/povertyplots.R
@@ -28,18 +28,25 @@ povbelow = subset(povstate, level == 'below.poverty.level')
 states <- map_data("state")
 povplot = merge(povbelow,states,by='region')
 povplot$percentage = povplot$total / povplot$state_total
+povplot$percentageind = NULL
+povplot$percentageind[povplot$percentage > .05 & povplot$percentage <= .07 ] = '.07  and below'
+povplot$percentageind[povplot$percentage > .07 & povplot$percentage <= .09 ] = '.07 - .09'
+povplot$percentageind[povplot$percentage > .09 & povplot$percentage <= .11 ] = '.09 - .11'
+povplot$percentageind[povplot$percentage > .11 & povplot$percentage <= .13 ] = '.11 - .13'
+povplot$percentageind[povplot$percentage > .13 ] = '.13 and above'
 
-
-
-# Plot percentage below poverty line
-qplot(long, lat, data=povplot, geom="polygon", order=order, 
-      group=group, fill = total/state_total)
-
-
-
-
-
-
+# Plot median income
+ggplot(povplot, aes(long,lat)) + 
+  geom_polygon(aes(long, lat, order=order, fill=percentageind, group=group)) +
+  theme_bw() + coord_map() + 
+  theme(axis.ticks = element_blank(), 
+        axis.text.x = element_blank(), 
+        axis.title.x=element_blank(), 
+        axis.text.y = element_blank(), 
+        axis.title.y=element_blank(), 
+        panel.grid = element_blank(), 
+        panel.border = element_blank()) +
+  geom_path(aes(long, lat, order=order, group=group))
 
 p = dPlot(x = "state", y = "freq", groups = "education", data = pov, type = "bar")
 p$xAxis(orderRule = "state")
